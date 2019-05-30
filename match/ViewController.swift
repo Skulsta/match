f6ef685f6357de2ec2ebee243a5768c517ad4ec6
Stian Skulstad <sti.sku@gmail.com>
2019-05-30 12:41:53 +0200

    Initial Commit

---
+++ match/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  match
+//
+//  Created by Stian on 30/05/2019.
+//  Copyright © 2019 Stian Skulstad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
